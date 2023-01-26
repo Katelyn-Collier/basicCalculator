7a21199e017c792a3a4de5ec6eb136e48245b019
Katelyn-Collier <katelyncollier14@gmail.com>
2023-01-25 23:29:15 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            do
+            {
+                int result;
+                Console.WriteLine("Enter number:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What operation would you like to perform? (+, -, /, *)");
+                string operation = Console.ReadLine();
+                Console.WriteLine("Enter nummber:");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                switch (operation)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine(result);
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine(result);
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine(result);
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        Console.WriteLine(result);
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, I don't know that input");
+                        break;
+                }
+                Console.ReadLine();
+                Console.WriteLine("Would you like to try another operation? (yes/no)");
+                value = Console.ReadLine();
+
+
+
+            }
+            while (value == "yes");
+
+            if (value == "no")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+ 
+
+        }
+    }
+}
